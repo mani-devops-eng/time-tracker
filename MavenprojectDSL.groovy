377901b625e2859ed05af3674909c17e932124bf
mani-devops-eng <75937999+mani-devops-eng@users.noreply.github.com>
2021-02-15 00:07:01 -0600

    Update MavenprojectDSL.groovy

--- MavenprojectDSL.groovy
+++ MavenprojectDSL.groovy
@@ -1,7 +1,7 @@
 job('My 1st job via DSL plugin') {
     description("this is my first job to test the 1st DSL groovy script code file to pacakge the project")
     scm {
-        github('https://github.com/mani-devops-eng/time-tracker.git', 'master')
+        github("https://github.com/mani-devops-eng/time-tracker.git", master)
     }
     triggers {
         scm('* * * * *')
